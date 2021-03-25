47f34998ba8a3ca33d8a20dcef51ba5c533a4467
licia03 <lgavina7@gmail.com>
2021-03-25 22:06:34 +0100

    enable ts declarations

--- src/luhnAlgorithm.ts
+++ src/luhnAlgorithm.ts
@@ -1,15 +1,20 @@
 // See https://it.wikipedia.org/wiki/Partita_IVA
-export default function luhnValidate(value: string) {
+
+export type LuhnValidateFunction = (value: string) => boolean;
+
+const luhnValidate: LuhnValidateFunction = (value) => {
   let arr = value
-    .replace(/\s+/g, '')
-    .split('')
+    .replace(/\s+/g, "")
+    .split("")
     .reverse()
-    .map(x => parseInt(x));
+    .map((x) => parseInt(x));
   let sum = arr.reduce(
     (acc, val, i) =>
       i % 2 === 0 ? acc + val : acc + (val * 2 > 9 ? val * 2 - 9 : val * 2),
-    0,
+    0
   );
 
   return sum % 10 === 0;
-}
+};
+
+export default luhnValidate;
